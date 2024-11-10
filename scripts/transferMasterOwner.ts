9a68f16d010819c87c84648eb9605ef8807ceede
Yuriy <berlandspw@gmail.com>
2024-11-10 11:54:49 +0500

    transfer owner script

---
+++ scripts/transferMasterOwner.ts
@@ -0,0 +1,26 @@
+import { Address, beginCell, toNano } from '@ton/core';
+import { NftCollection } from '../wrappers/UsualNftCollection';
+import { NetworkProvider } from '@ton/blueprint';
+import { SimpleNftMaster } from '../wrappers/SimpleNftMaster';
+
+export async function run(provider: NetworkProvider) {
+    const ui = provider.ui();
+    const contractAddress = await ui.input('Contract address:');
+    const newOwner = await ui.input('New owner address:');
+
+    const contract = provider.open(SimpleNftMaster.fromAddress(Address.parse(contractAddress)));
+
+    await contract.send(
+        provider.sender(),
+        {
+            value: toNano('0.6'),
+        },
+        {
+            $$type: "TransferOwner",
+            new_owner: Address.parse(newOwner)
+        }
+    );
+
+    const newData = await contract.getGetMasterData();
+    console.log("Updated data:", newData);
+}
