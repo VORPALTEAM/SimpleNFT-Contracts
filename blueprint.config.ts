f8a8bbc351c55f95cbd3f3909378800470446878
Yuriy <berlandspw@gmail.com>
2024-11-12 10:58:03 +0500

    contract fix

--- blueprint.config.ts
+++ blueprint.config.ts
@@ -3,11 +3,9 @@ import { Config } from '@ton/blueprint';
 /*
 export const config: Config = {
     network: {
-        endpoint: 'https://toncenter.com/api/v2/jsonRPC',
-        type: 'mainnet',
-        version: 'v4',
-        key: process.env.API_KEY,
-        
+        endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+        type: 'testnet',
+        key: process.env.API_KEY
     },
 
 } */
